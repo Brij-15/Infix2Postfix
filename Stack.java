e852dcfe472ccdb82ed097948f8a522dffa2904e
Brij Patel <brij.15@live.ca>
2017-02-20 19:59:02 -0500

    Start of stack reference based

--- Stack.java
+++ Stack.java
@@ -1,1 +1,13 @@
 //Brij Patel 101008373 Java Stack Reference Based Implementation
+public class Stack{
+private Node top;
+private int items;
+
+public Stack(){
+  top = null;
+  items = 0;
+}
+
+
+
+}
