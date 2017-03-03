434c7f1d8d2261eaf926905fb8adc2688af8d8e8
Brij Patel <brij.15@live.ca>
2017-03-03 12:39:43 -0500

    num >9 working need to get calculation working, need to figure out how to convert int to char, right now it goes to ASCII

--- Infix2Postfix.java
+++ Infix2Postfix.java
@@ -15,6 +15,7 @@ public static void main (String[] args){
 @SuppressWarnings("static-access")
 public static StringBuilder convert(String input){
 	Stack holder = new Stack();
+	Stack calcStk = new Stack();
 	Character plus = '+';
 	Character minus = '-';
 	Character multi = '*';
@@ -23,10 +24,23 @@ public static StringBuilder convert(String input){
 	Character Rbrac = ')';
 	char temp;
 	StringBuilder postStr = new StringBuilder();
+	StringBuilder calcStr = new StringBuilder();
+	
 	for(int i = 0; i < input.length(); i++){
 		char c = input.charAt(i);
 		if(c != plus && c != minus && c != multi && c != div && c != Lbrac && c != Rbrac){
 			postStr.append(c);
+			try{ //if next char is a number then append it on to string b/c its greater than 9
+				char t = input.charAt(i+1);
+			if(t != plus && t != minus && t != multi && t != div && t != Lbrac && t != Rbrac){
+				postStr.append(t +" ");
+				i = i + 1;
+			} else {
+				postStr.append(" ");//adds space between numbers 
+			}
+			} catch (StringIndexOutOfBoundsException s){
+				//postStr.append(" ");
+			}
 		}
 		else if(c == Lbrac){
 			holder.push(new Node(c));
@@ -69,6 +83,52 @@ public static StringBuilder convert(String input){
 		postStr.append(temp);
 		holder.pop();
 	}
+		for(int j = 0; j < postStr.length(); j++){
+			calcStr.append(postStr.charAt(j));
+		}
+		for(int k = 0; k < calcStr.length(); k++){
+			char a = calcStr.charAt(k);
+			if(a == plus || a == minus || a == multi || a == div){
+				if(a == plus){
+					int num1 = calcStk.peek();
+					calcStk.pop();
+					int num2 = calcStk.peek();
+					calcStk.pop();
+					int result = num2 + num1;
+					calcStk.push(new Node((char) result)); //problem here need way to conver int to char, rn it converts it to ascii 
+				}
+				else if(a == minus){
+					int num1 = calcStk.peek();
+					calcStk.pop();
+					int num2 = calcStk.peek();
+					calcStk.pop();
+					int result = num2 - num1;
+					calcStk.push(new Node((char) result));
+				}
+				else if(a == multi){
+					int num1 = calcStk.peek();
+					calcStk.pop();
+					int num2 = calcStk.peek();
+					calcStk.pop();
+					int result = num2 * num1;
+					calcStk.push(new Node((char) result));
+				}
+				else if(a == div){
+					int num1 = calcStk.peek();
+					calcStk.pop();
+					int num2 = calcStk.peek();
+					calcStk.pop();
+					int result = num2 / num1;
+					calcStk.push(new Node((char) result));
+				}
+			} else { // a is a number
+				calcStk.push(new Node(a));
+			}
+			System.out.println(" ");
+			calcStk.print();
+			System.out.println("");
+		}
 	return postStr;
 }
+
 }
